6adbb796a9cb960d3b3eed99f88fc7f85baa16d3
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-03-22 14:45:13 +0100

    Ancien code

---
+++ models.py
@@ -0,0 +1,23 @@
+class Router:
+    pass
+
+
+class Interface:
+    pass
+
+class Igp:
+    pass
+class Neighbor:
+    pass
+class Network:
+    pass
+class AdjRib:
+    pass
+class RouteMap:
+    pass
+class AsPathAccessList:
+    pass
+class PrefixList:
+    pass
+class Community:
+    pass
